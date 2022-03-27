44f39bb8db0cd9176082da1e49fbbf58598bd2a1
Josa Gesell <josa@gesell.me>
2022-03-27 20:47:38 +0200

    fix: compact formatting

--- lua/jg/quickfix.lua
+++ lua/jg/quickfix.lua
@@ -1,19 +1,21 @@
 local M = {}
 
-local limit_default = 96
+local fname_len_default = 96
 
-local function pad_right(str, len)
-  return string.sub(str .. string.rep(' ', len), 0, len)
-end
-
-local function format_fname(fname, limit)
-  -- char in fname may occur more than 1 width, ignore this issue in order to
-  -- keep performance
-  if #fname <= limit then
-    return pad_right(fname, limit)
+local function format_fname(fname, len)
+  if vim.fn.strchars(fname) <= len then
+    return vim.fn.strcharpart(fname .. string.rep(' ', len), 0, len)
   end
 
-  return '…' .. fname:sub(1 - limit)
+  return '…' .. fname:sub(1 - len)
+end
+
+local function format_text(type, text)
+  return (type and type:sub(1, 1):upper() .. ' ' or '') .. text
+end
+
+local function format_pos(lnum, lnum_len, col, col_len)
+  return ('%' .. lnum_len .. 'd:%-' .. col_len .. 'd'):format(lnum, col)
 end
 
 local function buf_fname(bufnr)
@@ -39,27 +41,45 @@ local function get_items(info)
   return vim.fn.getloclist(info.winid, { id = info.id, items = 0 }).items
 end
 
+local function max_len(len, str)
+  return math.max(len, vim.fn.strchars(str))
+end
+
 function M.format(info)
-  local ret = {}
+  local lines = {}
+  local items = get_items(info)
 
-  for _, e in ipairs(get_items(info)) do
-    local str
+  local fname_len = 0
+  local lnum_len = 0
+  local col_len = 0
 
+  for _, e in ipairs(items) do
     if e.valid == 1 then
-      local fname = format_fname(buf_fname(e.bufnr), limit_default)
+      e.fname = buf_fname(e.bufnr)
+      e.lnum = e.lnum > 99999 and -1 or e.lnum
+      e.col = e.col > 999 and -1 or e.col
 
-      local lnum = e.lnum > 99999 and -1 or e.lnum
-      local col = e.col > 999 and -1 or e.col
-      local qtype = e.type == '' and '' or ' ' .. e.type:sub(1, 1):upper()
-
-      str = ('%s │%5d:%-3d│%s %s'):format(fname, lnum, col, qtype, e.text)
-    else
-      str = e.text
+      fname_len = max_len(fname_len, e.fname)
+      lnum_len = max_len(lnum_len, e.lnum)
+      col_len = max_len(col_len, e.col)
     end
-    table.insert(ret, str)
   end
 
-  return ret
+  fname_len = math.min(fname_len, fname_len_default)
+
+  for _, e in ipairs(items) do
+    if e.valid == 1 then
+      e.text = ('%s │ %s │ %s'):format(
+        format_fname(e.fname, fname_len),
+        format_pos(e.lnum, lnum_len, e.col, col_len),
+        format_text(e.type, e.text)
+      )
+    end
+
+    table.insert(lines, e.text)
+  end
+
+  return lines
 end
 
 return M
